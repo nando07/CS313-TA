a9143dee86a300fde92f4b9c5548d90729ad011c
Nando <redchameleonapps@gmail.com>
2018-06-01 18:12:22 -0500

    Added Files

--- web/scriptureDetails.php
+++ web/scriptureDetails.php
@@ -52,7 +52,7 @@ catch (PDOException $ex)
 
 </div>
 <div>
-  <form name="newScriptureForm" action="scriptureDetails.php">
+  <form name="newScriptureForm" action="newScripture.php">
    <label>New Topic:</label>
     <input type='checkbox' name='newTopicCheck' id='newTopicCheck'>
     <input type="text" id="" name="newTopic" placeholder="New Topic"/>
@@ -72,54 +72,7 @@ catch (PDOException $ex)
 </form>
 </div>
 
-   <?php
-
-// get the data from the POST
-$book = $_POST['newBook'];
-$chapter = $_POST['newChapter'];
-$verse = $_POST['newVerse'];
-$content = $_POST['newContent'];
-$newTopic = $_POST['newTopic'];
-
-require("connectDB.php");
-$db = get_db();
-    
-    $query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
-	$statement = $db->prepare($query);
-	// Now we bind the values to the placeholders. This does some nice things
-	// including sanitizing the input with regard to sql commands.
-	$statement->bindValue(':book', $book);
-	$statement->bindValue(':chapter', $chapter);
-	$statement->bindValue(':verse', $verse);
-	$statement->bindValue(':content', $content);
-	$statement->execute();
-	// get the new id
-	$scriptureId = $db->lastInsertId("scripture_id_seq");
-    
-    
-    $query = 'INSERT INTO topic(name) VALUES(:name)';
-	$statement = $db->prepare($query);
-    $statement->bindValue(':name', $newTopic);
-    $statement->execute();
-    
-	// Now go through each topic id in the list from the user's checkboxes
-	foreach ($topicIds as $topicId)
-	{
-		$statement = $db->prepare('INSERT INTO scripture_topic(scriptureid, topicid) VALUES(:scriptureId, :topicId)');
-		// Then, bind the values
-		$statement->bindValue(':scriptureId', $scriptureId);
-		$statement->bindValue(':topicId', $topicId);
-		$statement->execute();
-	}
-}
-catch (Exception $ex)
-{
-	// Please be aware that you don't want to output the Exception message in
-	// a production environment
-	echo "Error with DB. Details: $ex";
-	die();
-}
-?>
+   
 
 </body>
 </html>
