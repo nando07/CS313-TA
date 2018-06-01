2a1d2fbc80fd00029fd6fd70221a2af409c1b79c
Nando <redchameleonapps@gmail.com>
2018-06-01 13:28:30 -0500

    Added Files

--- web/teach06form.php
+++ web/teach06form.php
@@ -48,7 +48,7 @@ try
 		// The trick here is that we need a unique id for each one. In this case,
 		// we use "chkTopics" followed by the id, so that it becomes something like
 		// "chkTopics1" and "chkTopics2", etc.
-		echo "<label for='topics$id'>$name</label><br />";
+		echo "<label for='topic$id'>$name</label><br />";
 		// put a newline out there just to make our "view source" experience better
 		echo "\n";
 	}
