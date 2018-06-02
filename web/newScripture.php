6b7d5c7de8ccb45ba9633d6aec81b36de5eff97b
Nando <redchameleonapps@gmail.com>
2018-06-01 22:39:21 -0500

    Added Files

--- web/newScripture.php
+++ web/newScripture.php
@@ -11,15 +11,15 @@ $newTopic = $_POST['newTopic'];
 require("connectDB.php");
 $db = get_db();
 //    
-//    $query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
-//	$statement = $db->prepare($query);
+$query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
+	$statement = $db->prepare($query);
 //	// Now we bind the values to the placeholders. This does some nice things
 //	// including sanitizing the input with regard to sql commands.
-//	$statement->bindValue(':book', $newBook);
-//	$statement->bindValue(':chapter', $newChapter);
-//	$statement->bindValue(':verse', $newVerse);
-//	$statement->bindValue(':content', $newContent);
-//	$statement->execute();
+	$statement->bindValue(':book', $newBook);
+	$statement->bindValue(':chapter', $newChapter);
+	$statement->bindValue(':verse', $newVerse);
+	$statement->bindValue(':content', $newContent);
+	$statement->execute();
 //	// get the new id
 //	$scriptureId = $db->lastInsertId("scripture_id_seq");
 
