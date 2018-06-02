2852c8daa944c0559785e0f0f33d630ba83c8992
Nando <redchameleonapps@gmail.com>
2018-06-01 20:00:58 -0500

    Added Files

--- web/newScripture.php
+++ web/newScripture.php
@@ -14,20 +14,22 @@ echo $newVerse;
 echo $newContent;
 echo $newTopic;
 
-//require("connectDB.php");
-//$db = get_db();
-//    
-//    $query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
-//	$statement = $db->prepare($query);
-//	// Now we bind the values to the placeholders. This does some nice things
-//	// including sanitizing the input with regard to sql commands.
-//	$statement->bindValue(':book', $newBook);
-//	$statement->bindValue(':chapter', $newChapter);
-//	$statement->bindValue(':verse', $newVerse);
-//	$statement->bindValue(':content', $newContent);
-//	$statement->execute();
-//	// get the new id
-//	$scriptureId = $db->lastInsertId("scripture_id_seq");
+require("connectDB.php");
+$db = get_db();
+    
+    $query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
+	$statement = $db->prepare($query);
+	// Now we bind the values to the placeholders. This does some nice things
+	// including sanitizing the input with regard to sql commands.
+	$statement->bindValue(':book', $newBook);
+	$statement->bindValue(':chapter', $newChapter);
+	$statement->bindValue(':verse', $newVerse);
+	$statement->bindValue(':content', $newContent);
+	$statement->execute();
+	// get the new id
+	$scriptureId = $db->lastInsertId("scripture_id_seq");
+
+
 //   
 //    $statement = $db->prepare('INSERT INTO topic(name) VALUES(:name)');
 //
