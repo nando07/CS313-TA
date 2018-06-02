f2c1415f6f3f71d15b43cc4c69c47a80fa0dc1ce
Nando <redchameleonapps@gmail.com>
2018-06-02 01:02:29 -0500

    Added Files

--- web/teach06.php
+++ web/teach06.php
@@ -46,6 +46,7 @@ catch (Exception $ex)
 	// Please be aware that you don't want to output the Exception message in
 	// a production environment
 	echo "Error with DB. Details: $ex";
+    echo $newTopic;
 	die();
 }
 // finally, redirect them to a new page to actually show the topics
